35aeac77da63ccc4e78097a675c549907b3aa85a
markc <markc@pageuppeople.com>
2018-05-15 08:30:10 +1000

    Fix errors in 20.exercies.hs
    Foldables were not correctly mappending the final argument
    when of the Foldable type.

--- exercises/20.exercises.hs
+++ exercises/20.exercises.hs
@@ -1,3 +1,5 @@
+module Exercises20 where
+
 
 data Constant a b = Constant b
 
@@ -14,10 +16,18 @@ data Three a b c = Three a b c
 instance Foldable (Three a b) where
     foldMap f (Three a b c) = f c
 
+
+data Three' a b =
+    Three' a b b
+
+instance Foldable (Three' a) where
+    foldMap f (Three' a b1 b2) = (f b1) `mappend` (f b2)
+
+
 data Four' a b = Four' a b b b
 
 instance Foldable (Four' a) where
-    foldMap f (Four' a b b' b'') = f b''
+    foldMap f (Four' a b1 b2 b3) = (f b1) `mappend` (f b2) `mappend` (f b3)
 
 
 filterF :: ( Applicative f

49d6002de22e67496303ff758e86dade9159b1f5
markc <markc@pageuppeople.com>
2018-08-15 17:49:10 +1000

    Basic version of 'Ord SemVer' working
    Overall seems to work.
    But pattern matching is not the best (can't reorder the matches).
    Also, special empty list handling is annoying.

--- exercises/24.semver.hs
+++ exercises/24.semver.hs
@@ -13,7 +13,14 @@ import Test.Hspec
 data NumberOrString =
     NOSS String
   | NOSI Integer
-    deriving (Eq, Ord, Show)
+    deriving (Eq, Show)
+
+instance Ord NumberOrString where
+    compare (NOSI i) (NOSI i') = compare i i'
+    compare (NOSS s) (NOSS s') = compare s s'
+    compare (NOSI _) (NOSS _) = GT
+
+data VersionNumber = VersionNumber Integer
 
 type Major = Integer
 type Minor = Integer
@@ -28,7 +35,38 @@ data SemVer =
         Patch 
         Release 
         Metadata
-    deriving (Eq, Ord, Show)
+    deriving (Eq, Show)
+
+
+instance Ord SemVer where
+    compare 
+        (SemVer major1 _ _ _ _) 
+        (SemVer major2 _ _ _ _) | major1 /= major2 = compare major1 major2
+    compare 
+        (SemVer major1 minor1 _ _ _) 
+        (SemVer major2 minor2 _ _ _) | ( major1 == major2 ) && (minor1 /= minor2) = compare minor1 minor2
+    compare 
+        (SemVer major1 minor1 patch1 _ _) 
+        (SemVer major2 minor2 patch2 _ _) 
+          | ( major1 == major2 ) && (minor1 == minor2) && (patch1 /= patch2)= compare patch1 patch2
+    -- TODO: if there is a pre-release, it makes it lesser than an equal one without pre-release.
+    
+    compare 
+        (SemVer major1 minor1 patch1 [] _) 
+        (SemVer major2 minor2 patch2 [] _) 
+          | ( major1 == major2 ) && (minor1 == minor2) && (patch1 == patch2) = EQ 
+
+    compare 
+        (SemVer major1 minor1 patch1 r1 _) 
+        (SemVer major2 minor2 patch2 [] _) 
+          | ( major1 == major2 ) && (minor1 == minor2) && (patch1 == patch2) = GT
+
+    compare 
+        (SemVer major1 minor1 patch1 r1 _) 
+        (SemVer major2 minor2 patch2 r2 _) 
+          | ( major1 == major2 ) && (minor1 == minor2) && (patch1 == patch2) = compare r1 r2
+
+    compare _ _  = EQ
 
 -- =========================================================
 -- Let the parsers begin...
@@ -104,4 +142,80 @@ describe "parseSemVer" $ do
         let semVer = "1.2.3-rere.r3r3.333+432.432.r3r3"
         let parsedSemVer = testParseSemVer semVer
         parsedSemVer `shouldBe` Success (SemVer 1 2 3 [NOSS "rere",NOSS "r3r3",NOSI 333] [NOSI 432,NOSI 432,NOSS "r3r3"])
+
+    -- it "must have a patch release numberr" $ do
+    --     let semVer = "1.2"
+    --     let parsedSemVer = testParseSemVer semVer
+    --     parsedSemVer `shouldBe` Failure _ 
+
+describe "NumberOrString ordinality" $ do
+    it "Integers are numerically compared" $ do
+        let a = NOSI 2
+        let b = NOSI 1
+        (compare a b) `shouldBe` GT
+
+    it "Strings are numerically lexically" $ do
+        let alpha = NOSS "alpha"
+        let beta = NOSS "beta"
+        (compare beta alpha) `shouldBe` GT
+
+    it "Integers are greater than strings" $ do
+        let int = NOSI 1
+        let str = NOSS "alpha"
+        (compare int str) `shouldBe` GT
+
+
+    it "An element is greater than no element" $ do
+        let a = [NOSS "alpha", NOSI 1]
+        let b = [NOSS "alpha"]
+        (compare a b) `shouldBe` GT
+
+describe "parseSemVer ordinality" $ do
+    it "2.0.0 > 1.0.0" $ do
+        let v2 = testParseSemVer "2.0.0"
+        let v1 = testParseSemVer "1.0.0"
+        (compare <$> v2 <*> v1) `shouldBe` Success (GT)
+
+    it "1.1.0 > 1.0.0" $ do
+        let v11 = testParseSemVer "1.1.0"
+        let v1 = testParseSemVer "1.0.0"
+        (compare <$> v11 <*> v1) `shouldBe` Success (GT)
+
+
+    it "1.0.1 > 1.0.0" $ do
+        let v101 = testParseSemVer "1.0.1"
+        let v1 = testParseSemVer "1.0.0"
+        (compare <$> v101 <*> v1) `shouldBe` Success (GT)
+
+    it "1.0.0 > 1.0.0-alpha" $ do
+        let v1 = testParseSemVer "1.0.0"
+        let v1alpha = testParseSemVer "1.0.0-alpha"
+        (compare <$> v1alpha <*> v1) `shouldBe` Success (GT)
+
+    it "1.0.0 = 1.0.0" $ do
+        let v1 = testParseSemVer "1.0.0"
+        let v1' = testParseSemVer "1.0.0"
+        (compare <$> v1 <*> v1') `shouldBe` Success (EQ)
+
+    it "1.0.0-2 > 1.0.0-1" $ do
+        let a = testParseSemVer "1.0.0-2"
+        let a' = testParseSemVer "1.0.0-1"
+        (compare <$> a <*> a') `shouldBe` Success (GT)
+
+
+    it "1.0.0-alpha2 > 1.0.0-alpha1" $ do
+        let a = testParseSemVer "1.0.0-alpha2"
+        let a' = testParseSemVer "1.0.0-alpha1"
+        (compare <$> a <*> a') `shouldBe` Success (GT)
+
+
+    it "1.0.0-alpha.2 > 1.0.0-alpha.1" $ do
+        let a = testParseSemVer "1.0.0-alpha.2"
+        let a' = testParseSemVer "1.0.0-alpha.1"
+        (compare <$> a <*> a') `shouldBe` Success (GT)
+
+    it "metadata does not affect the ordering" $ do
+        let v1 = testParseSemVer "1.0.0-alpha+1"
+        let v1' = testParseSemVer "1.0.0-alpha+2"
+        (compare <$> v1 <*> v1') `shouldBe` Success (EQ)
 
